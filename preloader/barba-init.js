96ed34365a8a047b59f5813db7d0303ef95060ed
red-holding <general@redprop.online>
2026-03-15 21:34:08 +0300

    Update barba-init.js

--- preloader/barba-init.js
+++ preloader/barba-init.js
@@ -4,7 +4,6 @@
  */
 (function () {
   function preventTransition(data) {
-    if (window.location.protocol === "file:") return true;
     var a = data.trigger;
     if (!a || !a.getAttribute("href")) return true;
     var raw = a.getAttribute("href");
@@ -25,7 +24,6 @@
   }
 
   function initBarbaTransitions() {
-    if (window.location.protocol === "file:") return false;
     if (typeof barba === "undefined") return false;
     if (!document.querySelector("[data-barba='wrapper']")) return false;
     if (!document.querySelector("[data-barba='container']")) return false;
